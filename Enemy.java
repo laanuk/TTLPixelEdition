526379dbf1a6f3109f763a3c44c8543217705e52
Kunaal Naik <laanuk13@gmail.com>
2014-07-30 19:02:17 -0700

    Update Enemy.java

--- Enemy.java
+++ Enemy.java
@@ -3,21 +3,35 @@ package Tilt;
 import java.awt.Color;
 import java.awt.Graphics2D;
 
-
-public class Dot {
-	public double radius = 10.0;
-	int x;
-	int y;
-	public Dot(int x, int y) {
-		this.x = x;
-		this.y = y;
+public class Enemy extends Dot {
+int espeed;
+public boolean cyanerds = false;
+	public Enemy(int x, int y, int s) {
+		super(x, y);
+		espeed = s;
 	}
 	
-	public void draw(Graphics2D g2d) {
-		if (x > 700) x = Game.WIDTH - 15;
-		if (y > 700) y = Game.HEIGHT - 15;
-		if (x < 0) x = 0;
-		if (y < 0) y = 0;
-		g2d.fillOval(x, y, (int) radius, (int) radius);
+	public void draw (Graphics2D g2d, int px, int py){
+		if (!cyanerds) {
+			g2d.setColor(Color.BLACK);
+		
+//		if (Math.abs(px-x) < 5 && Math.abs(py-y) < 5) System.exit(0);
+		
+			if (px > x) x+= espeed; 
+			else x-= espeed;
+			if (py > y) y+= espeed;
+			else y-= espeed;
+		
+			if (x > 700) x = Game.WIDTH - 15;
+			if (y > 700) y = Game.HEIGHT - 15;
+			if (x < 0) x = 0;
+			if (y < 0) y = 0;
+		
+			g2d.fillOval(x, y, (int) radius, (int) radius);
+		}
+		if (cyanerds) {
+			x = -100; y = -100;
+		}
 	}
+
 }
